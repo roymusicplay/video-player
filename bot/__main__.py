b1e5d6e46fb79e2b10202c481abe82236ef6f13e
ABHINAS ROY <85425565+roymusicplay@users.noreply.github.com>
2021-09-05 08:19:28 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -35,9 +35,9 @@ if not os.path.isdir("./downloads"):
 
 Bot.start()
 User.start()
-print("\nVideo Player Bot Started, Join @AsmSafone!")
+print("\nVideo Player Bot Started, Join @ABOUT_ABHINAS!")
 
 idle()
 Bot.stop()
 User.stop()
-print("\nVideo Player Bot Stopped, Join @AsmSafone!")
+print("\nVideo Player Bot Stopped, Join @ABOUT_ABHINAS!")
